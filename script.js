b219dbf681086a5ff9207632022a6fddc98aa58b
Henrique Lecce <henrique.lecce.73@gmail.com>
2025-05-06 16:04:05 -0300

    new poertifolio - start

---
+++ script.js
@@ -0,0 +1,19 @@
+
+/*
+// Animação de cor no hover do nome
+const span = document.getElementById('logo-span');
+const colors = ['var(--first-color)', 'var(--second-color)', 'var(--third-color)'];
+let hoverCount = 0;
+
+span.addEventListener('mouseenter', () => {
+    const nextColor = colors[hoverCount % colors.length];
+    span.style.color = nextColor;
+    hoverCount++;
+});
+
+span.addEventListener('mouseleave', () => {
+    span.style.color = 'var(--main-color)';
+});
+*/
+
+
